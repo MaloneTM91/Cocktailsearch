5a81f882e3574a7ea38f0b96cd413ea152e78e15
MaloneTM91 <timalone1991@gmail.com>
2022-08-25 15:50:43 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Have you ever wanted to have an easy search option for any cocktail you
+        could imagine? Me too! My memory is terrible! That's why this is the
+        perfect resource for you. Now, it won't stock the bar or make the drink
+        for you, but at least it'll quickly and easily do the legwork for you.
+        Happy mixing!
+      </p>
+    </section>
+  );
+}
