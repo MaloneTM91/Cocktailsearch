a40ced57ee45522b42c9345cf31c1aa13db62c2f
MaloneTM91 <101269413+MaloneTM91@users.noreply.github.com>
2022-08-26 15:36:51 -0500

    Add files via upload

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,15 +4,14 @@ export default function Error() {
   return (
     <section className="error-page section">
       <div className="error-container">
-        <h1>oops! it's a dead end</h1>
+        <h1>
+          oops! it's a dead end! this is a demonstration of a purposeful error
+          page. this error will appear every time the user navigates away from
+          an established page. now click the home button and find a tasty drink!
+        </h1>
         <Link to="/" className="btn btn-primary">
           back home
         </Link>
-        <h2>
-          this is a demonstration of a purposeful error page. this error will
-          appear every time the user navigates away from an established page.
-          now click the home button and find a tasty drink!
-        </h2>
       </div>
     </section>
   );
